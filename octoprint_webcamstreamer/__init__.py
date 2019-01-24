f82991da80963890c1194e2f6900602bb6d40f5c
Adi Linden <adi@adis.ca>
2019-01-23 19:27:15 -0600

    Brought in relevant parts from adilinden-oss/octoprint-youtubelive as baseline to work from.

--- octoprint_webcamstreamer/__init__.py
+++ octoprint_webcamstreamer/__init__.py
@@ -10,50 +10,159 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+from octoprint.server import user_permission
+import docker
 
-class WebcamstreamerPlugin(octoprint.plugin.SettingsPlugin,
-                           octoprint.plugin.AssetPlugin,
-                           octoprint.plugin.TemplatePlugin):
+class WebcamstreamerPlugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.SimpleApiPlugin,
+    octoprint.plugin.EventHandlerPlugin):
 
-	##~~ SettingsPlugin mixin
+    def __init__(self):
+        self.client = docker.from_env()
+        self.container = None
+    
+    ##~~ StartupPlugin
+    
+    def on_after_startup(self):
+        self._logger.info("OctoPrint-WebcamStreamer loaded! Checking stream status.")
+        try:
+            self.container = self.client.containers.get('WebcamStreamer')
+            self._logger.info("%s is streaming " % self.container.name)
+            self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
+        except Exception, e:
+            self._logger.error(str(e))
+            self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    ##~~ TemplatePlugin
+    
+    def get_template_configs(self):
+        return [dict(type="settings",custom_bindings=False)]
 
-	##~~ AssetPlugin mixin
+    ##~~ SettingsPlugin mixin
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/webcamstreamer.js"],
-			css=["css/webcamstreamer.css"],
-			less=["less/webcamstreamer.less"]
-		)
+    def get_settings_defaults(self):
+        return dict(
+            # put your plugin's default settings here
+            channel_id="",
+            stream_url="rtmp://a.rtmp.youtube.com/live2",
+            stream_id="",
+            webcam_url="",
+            streaming=False,
+            auto_start=False
+        )
 
-	##~~ Softwareupdate hook
+    ##~~ AssetPlugin mixin
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			webcamstreamer=dict(
-				displayName="Webcamstreamer Plugin",
-				displayVersion=self._plugin_version,
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/webcamstreamer.js"],
+            css=["css/webcamstreamer.css"],
+            less=["less/webcamstreamer.less"]
+        )
 
-				# version check: github repository
-				type="github_release",
-				user="adilinden-oss",
-				repo="octoprint-webcamstreamer",
-				current=self._plugin_version,
+    ##~~ SimpleApiPlugin
+    
+    def get_api_commands(self):
+        return dict(startStream=[],stopStream=[],checkStream=[])
+        
+    def on_api_command(self, command, data):
+        if not user_permission.can():
+            from flask import make_response
+            return make_response("Insufficient rights", 403)
+        
+        if command == 'startStream':
+            self._logger.info("Start stream command received.")
+            self.startStream()
 
-				# update method: pip
-				pip="https://github.com/adilinden-oss/octoprint-webcamstreamer/archive/{target_version}.zip"
-			)
-		)
+        if command == 'stopStream':
+            self._logger.info("Stop stream command received.")
+            self.stopStream()
+
+        if command == 'checkStream':
+            self._logger.info("Checking stream status.")
+            if self.container:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
+            else:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+
+    ##-- EventHandlerPlugin
+    
+    def on_event(self, event, payload):
+        if event == "PrintStarted" and self._settings.get(["auto_start"]):
+            self.startStream()
+            
+        if event in ["PrintDone","PrintCancelled"] and self._settings.get(["auto_start"]):
+            self.stopStream()
+
+    ##-- Utility Functions
+    
+    def startStream(self):
+        if not self.container:
+            filters = []
+            if self._settings.global_get(["webcam","flipH"]):
+                filters.append("hflip")
+            if self._settings.global_get(["webcam","flipV"]):
+                filters.append("vflip")
+            if self._settings.global_get(["webcam","rotate90"]):
+                filters.append("transpose=cclock")
+            if len(filters) == 0:
+                filters.append("null")
+            try:
+                self.container = self.client.containers.run(
+                    "adilinden/rpi-stream:latest",
+                    command=[
+                        "octopi-youtubelive",
+                        self._settings.get(["webcam_url"]),
+                        self._settings.get(["stream_url"]),
+                        self._settings.get(["stream_id"]),
+                        ",".join(filters)],
+                        detach=True,
+                        privileged=True,
+                        name="WebcamStreamer",
+                        auto_remove=True)
+                self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
+            except Exception, e:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
+        return
+        
+    def stopStream(self):
+        if self.container:
+            try:
+                self.container.stop()
+                self.container = None
+                self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+            except Exception, e:
+                self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
+        else:
+            self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=False))
+
+    ##~~ Softwareupdate hook
+
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            webcamstreamer=dict(
+                displayName="Webcamstreamer Plugin",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="adilinden-oss",
+                repo="octoprint-webcamstreamer",
+                current=self._plugin_version,
+
+                # update method: pip
+                pip="https://github.com/adilinden-oss/octoprint-webcamstreamer/archive/{target_version}.zip"
+            )
+        )
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
@@ -62,11 +171,11 @@ class WebcamstreamerPlugin(octoprint.plugin.SettingsPlugin,
 __plugin_name__ = "Webcamstreamer Plugin"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = WebcamstreamerPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = WebcamstreamerPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
 
--- octoprint_webcamstreamer/static/js/webcamstreamer.js
+++ octoprint_webcamstreamer/static/js/webcamstreamer.js
@@ -4,26 +4,141 @@
  * Author: Adi Linden
  * License: AGPLv3
  */
-$(function() {
+$(function () {
     function WebcamstreamerViewModel(parameters) {
         var self = this;
+        
+        self.settingsViewModel = parameters[0];
+        self.channel_id = ko.observable();
+        self.stream_id = ko.observable();
+        self.streaming = ko.observable();
+        self.processing = ko.observable(false);
+        self.view_url = ko.observable();
+        self.icon = ko.pureComputed(function() {
+            var icons = [];
+            if (self.streaming() && !self.processing()) {
+                icons.push('icon-stop');
+            } 
+            
+            if (!self.streaming() && !self.processing()){
+                icons.push('icon-play');
+            }
+            
+            if (self.processing()) {
+                icons.push('icon-spin icon-spinner');
+            } 
+            
+            return icons.join(' ');
+        });
+        self.btnclass = ko.pureComputed(function() {
+            return self.streaming() ? 'btn-primary' : 'btn-danger';
+        });
+                                    
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        // This will get called before the WebcamstreamerViewModel gets bound to the DOM, but after its depedencies have
+        // already been initialized. It is especially guaranteed that this method gets called _after_ the settings
+        // have been retrieved from the OctoPrint backend and thus the SettingsViewModel been properly populated.
+        self.onBefireBinding = function () {
+            self.channel_id(self.settingsViewModel.settings.plugins.webcamstreamer.channel_id());
+            self.stream_id(self.settingsViewModel.settings.plugins.webcamstreamer.stream_id());
+        };
 
-        // TODO: Implement your plugin's view model here.
+        self.onEventSettingsUpdated = function (payload) {            
+            self.channel_id(self.settingsViewModel.settings.plugins.webcamstreamer.channel_id());
+            self.stream_id(self.settingsViewModel.settings.plugins.webcamstreamer.stream_id());
+        };
+        
+        self.onAfterBinding = function() {
+            $.ajax({
+                    url: API_BASEURL + "plugin/webcamstreamer",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify({
+                        command: "checkStream"
+                    }),
+                    contentType: "application/json; charset=UTF-8"
+                })
+        }
+        
+        self.onTabChange = function(next, current) {
+            if(next == '#tab_plugin_webcamstreamer'){
+                if(self.settingsViewModel.settings.webcam.streamRatio() == '4:3'){
+                    $('#webcamstreamer_wrapper').css('padding-bottom','75%');
+                }
+                self.view_url('https://www.youtube.com/embed/live_stream?channel=' + self.settingsViewModel.settings.plugins.webcamstreamer.channel_id());
+            } else {
+                self.view_url('');
+            }
+        }
+        
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "webcamstreamer") {
+                return;
+            }
+            
+            if(data.error) {
+                new PNotify({
+                            title: 'Webcam Streamer Error',
+                            text: data.error,
+                            type: 'error',
+                            hide: false,
+                            buttons: {
+                                closer: true,
+                                sticker: false
+                            }
+                            });
+            }
+            
+            if(data.status) {
+                if(data.streaming == true) {
+                    self.streaming(true);
+                } else {
+                    self.streaming(false);
+                }
+                
+            }
+            
+            self.processing(false);
+        };
+        
+        self.toggleStream = function() {
+            self.processing(true);
+            if (self.streaming()) {
+                $.ajax({
+                    url: API_BASEURL + "plugin/webcamstreamer",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify({
+                        command: "stopStream"
+                    }),
+                    contentType: "application/json; charset=UTF-8"
+                })
+            } else {
+                $.ajax({
+                    url: API_BASEURL + "plugin/webcamstreamer",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify({
+                        command: "startStream"
+                    }),
+                    contentType: "application/json; charset=UTF-8"
+                })
+            }
+        }
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    OCTOPRINT_VIEWMODELS.push({
-        construct: WebcamstreamerViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-        // Elements to bind to, e.g. #settings_plugin_webcamstreamer, #tab_plugin_webcamstreamer, ...
-        elements: [ /* ... */ ]
-    });
+    // This is how our plugin registers itself with the application, by adding some configuration information to
+    // the global variable ADDITIONAL_VIEWMODELS
+    ADDITIONAL_VIEWMODELS.push([
+            // This is the constructor to call for instantiating the plugin
+            WebcamstreamerViewModel,
+
+            // This is a list of dependencies to inject into the plugin, the order which you request here is the order
+            // in which the dependencies will be injected into your view model upon instantiation via the parameters
+            // argument
+            ["settingsViewModel"],
+
+            // Finally, this is the list of all elements we want this view model to be bound to.
+            [("#tab_plugin_webcamstreamer")]
+        ]);
 });
