6ff4626deb71abd5dc6a2b8fd0ece6415abd2082
Adi Linden <adi@adis.ca>
2019-01-24 02:18:31 +0000

    Added docker dependency.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/adilinden-oss/octoprint-webcamstreamer"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["docker==2.7.0"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
